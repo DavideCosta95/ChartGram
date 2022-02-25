7d9fd485e9b61f87eeacd65e7c34dc4271bdecb6
Gren <dashish95@gmail.com>
2022-02-25 14:49:46 +0100

    Added new localization strings

--- src/main/java/chartgram/config/Language.java
+++ src/main/java/chartgram/config/Language.java
@@ -11,4 +11,20 @@ public class Language {
 	@JsonProperty("non_command_text")
 	@NonNull
 	private String nonCommandText;
+
+	@JsonProperty("private_command_not_allowed_text")
+	@NonNull
+	private String privateCommandNotAllowedText;
+
+	@JsonProperty("unknown_command_text")
+	@NonNull
+	private String unknownCommandText;
+
+	@JsonProperty("must_be_admin_text")
+	@NonNull
+	private String mustBeAdminText;
+
+	@JsonProperty("link_sent_in_pvt_text")
+	@NonNull
+	private String linkSentInPvtText;
 }
