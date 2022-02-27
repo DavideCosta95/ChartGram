5d1fa678fd43943d56d8335f8ee10a20f2ed8e4a
Gren <dashish95@gmail.com>
2022-02-27 14:16:15 +0100

    Added test mode for home controller

--- src/main/java/chartgram/webapp/HomeController.java
+++ src/main/java/chartgram/webapp/HomeController.java
@@ -1,5 +1,6 @@
 package chartgram.webapp;
 
+import chartgram.config.Configuration;
 import chartgram.telegram.TelegramController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,41 +23,47 @@ public class HomeController {
 	private static final String ERROR_PAGE = "error";
 	private static final String MODEL_ATTRIBUTE_ERROR_NAME = "error_type";
 	private final TelegramController telegramController;
+	private final boolean test;
 
 	@Autowired
-	private HomeController(TelegramController telegramController) {
+	private HomeController(Configuration configuration, TelegramController telegramController) {
 		this.telegramController = telegramController;
+		this.test = configuration.isTest();
 	}
 
 	@GetMapping("/{groupId}")
 	public String home(@PathVariable("groupId") String groupId, @RequestParam("authorization") Optional<String> authorization, Model model, HttpServletRequest request, HttpServletResponse response) {
-		if (authorization.isEmpty()) {
-			response.setStatus(401);
-			model.addAttribute(MODEL_ATTRIBUTE_ERROR_NAME, "Missing authorization token");
-			return ERROR_PAGE;
-		}
+		UUID authorizationUUID;
+		if (!test) {
+			if (authorization.isEmpty()) {
+				response.setStatus(401);
+				model.addAttribute(MODEL_ATTRIBUTE_ERROR_NAME, "Missing authorization token");
+				return ERROR_PAGE;
+			}
 
-		UUID authorizationUUID = null;
-		try {
-			authorizationUUID = UUID.fromString(authorization.get());
-		} catch (IllegalArgumentException e) {
-			response.setStatus(400);
-			log.info("Malformed authorization UUID={}", authorization.get());
-			model.addAttribute(MODEL_ATTRIBUTE_ERROR_NAME, "Malformed authorization token");
-			return ERROR_PAGE;
-		}
+			try {
+				authorizationUUID = UUID.fromString(authorization.get());
+			} catch (IllegalArgumentException e) {
+				response.setStatus(400);
+				log.info("Malformed authorization UUID={}", authorization.get());
+				model.addAttribute(MODEL_ATTRIBUTE_ERROR_NAME, "Malformed authorization token");
+				return ERROR_PAGE;
+			}
 
-		Long groupTelegramId = telegramController.getGroupIdByAuthorizedUserUUID(authorizationUUID);
-		if (groupTelegramId == null) {
-			response.setStatus(403);
-			log.info("Unrecognized user. Authorization={}", authorizationUUID);
-			model.addAttribute(MODEL_ATTRIBUTE_ERROR_NAME, "Unrecognized user");
-			return ERROR_PAGE;
+			Long groupTelegramId = telegramController.getGroupIdByAuthorizedUserUUID(authorizationUUID);
+			if (groupTelegramId == null) {
+				response.setStatus(403);
+				log.info("Unrecognized user. Authorization={}", authorizationUUID);
+				model.addAttribute(MODEL_ATTRIBUTE_ERROR_NAME, "Unrecognized user");
+				return ERROR_PAGE;
+			}
 		}
-
+		else {
+			authorizationUUID = UUID.randomUUID();
+		}
 		request.getSession().setAttribute("authorization_token", authorizationUUID.toString());
 		model.addAttribute("authorization_token", authorizationUUID.toString());
 		model.addAttribute("group_id", groupId);
-		return "index";
+		return "charts";
 	}
 }
